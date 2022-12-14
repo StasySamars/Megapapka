25d9a956a2a6109d164aafd57d004f62cebc8701
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-15 00:35:23 +0300

    любимчики в деле

---
+++ lubimchiki/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Кто ты, смертный?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "настя")
+{
+    Console.WriteLine ("Моя дорогая мамочка, я рада тебя видеть!");
+}
+else 
+{
+    Console.Write("Аааа, это ты...кожанный, все еще носишь имя ");
+    Console.WriteLine(username);
+}

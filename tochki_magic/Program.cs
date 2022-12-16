aab769d7a815997bceee696e01a893f63335c122
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-16 16:03:27 +0300

    треуглоьник

--- tochki_magic/Program.cs
+++ tochki_magic/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 25,
-    xc = 80, yc = 25;
+int xa = 30, ya = 1, 
+    xb = 1, yb = 20,
+    xc = 60, yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -13,12 +13,12 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = xb;
+int x = xa, y=xb ;
 int count = 0;
 
-while (count < 10000)
+while(count < 10000)
 {
-    int what = new Random().Next(0, 3);
+    int what = new Random().Next(0, 3); //0 1 2
     if (what == 0)
     {
         x = (x + xa) / 2;
@@ -29,13 +29,12 @@ while (count < 10000)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
-
     }
+
     if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
-
     }
 
     Console.SetCursorPosition(x, y);
